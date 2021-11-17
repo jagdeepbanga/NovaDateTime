0c3f1901813e13114175e96396073cf6fe92324f
fleader64 <fleader64@gmail.com>
2021-11-17 10:40:56 +0300

    Fix show DateTime help text

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,5 +1,5 @@
 <template>
-    <default-field :field="field" :errors="errors" :show-help-text="showHelpText">
+    <default-field :field="field" :errors="errors" :show-help-text="field.helpText != null">
         <template slot="field">
             <div class="flex items-center">
                 <form-nova-date-time-picker
